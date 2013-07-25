1b0851d97824e3af1210a366a694f22d25e1191a
tangentstorm <michal.wallace@gmail.com>
2013-07-25 11:16:01 -0500

    vue2svg.py : spike of a data extractor for vue files.. ('2svg' part still forthcoming)

---
+++ spike/vue2svg.py
@@ -0,0 +1,56 @@
+import os, glob, io
+from lxml import etree
+
+nsmap = {
+    'xsi':"http://www.w3.org/2001/XMLSchema-instance"
+}
+
+def xp(tree, path):
+    match = tree.xpath(path, namespaces=nsmap)
+    return match[0] if match else ''
+
+
+def walk(tree, sep, indent):
+    """
+    walk the tree recursively, extracting node data
+    """
+    children = tree.xpath('child')
+    for child in children:
+        print('> ' * indent +
+              sep.join(xp(child, path) for path in [
+                    '@ID',
+                    '@xsi:type',
+                    'shape/@xsi:type',
+                    '@x',
+                    '@y',
+                    '@width',
+                    '@height',
+                    '@label',
+                    '@layerID',
+                    'fillColor/text()',
+                    'strokeWidth/text()',
+                    'strokeColor/text()',
+                    'textColor/text()',
+                    'font/text()',
+                    'ID1/text()',
+                    'ID2/text()',
+                    'point1/@x',
+                    'point1/@y',
+                    'point2/@x',
+                    'point2/@y',
+                    ]))
+        walk(child, sep, indent+1)
+
+
+for filename in sorted(glob.glob('*.vue')):
+
+    # vue files are not valid xml because the doctype is misplaced.
+    # so to fix, just strip off the opening comments:
+    data = open(filename, 'r').read()
+    data = data[data.find('<?xml'):]
+
+    vue = etree.parse(io.BytesIO(bytes(data, 'ascii')))
+
+    print('##', filename, '#' * (60-len(filename)))
+    walk(vue, '; ', 0)
+
