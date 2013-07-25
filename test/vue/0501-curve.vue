cdca1023496bcec016725fb06b02be88dd3f4440
sabren <michal.wallace@gmail.com>
2013-07-25 10:38:22 -0500

    added some curved edges

---
+++ test/vue/0501-curve.vue
@@ -0,0 +1,114 @@
+<!-- Tufts VUE 3.2.2 concept-map (05-edge-curved.vue) 2013-07-25 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Thu Jul 25 10:36:42 CDT 2013 by michal on platform Windows 7 6.1 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built May 23 2013 at 2146 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="05-edge-curved.vue" created="1374752385383" x="0.0" y="0.0"
+    width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1374766602349" size="6310"
+        spec="C:\ver\scenetool\test\vue\05-edge-curved.vue" type="1" xsi:type="URLResource">
+        <title>05-edge-curved.vue</title>
+        <property key="File" value="C:\ver\scenetool\test\vue\05-edge-curved.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/15a18bd7c0a8380100ded21c3300c69e</URIString>
+    <child ID="6" label="sub" layerID="1" created="1374752395896"
+        x="181.0" y="32.0" width="32.0" height="23.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <fillColor>#7F7F7F</fillColor>
+        <strokeColor>#8C8F72</strokeColor>
+        <textColor>#FFFFFF</textColor>
+        <font>Arial-bold-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/15a18bd8c0a8380100ded21ca4348ea0</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="8" label="obj" layerID="1" created="1374752403149"
+        x="183.0" y="105.5" width="28.0" height="23.0" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <fillColor>#7F7F7F</fillColor>
+        <strokeColor>#8C8F72</strokeColor>
+        <textColor>#FFFFFF</textColor>
+        <font>Arial-bold-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/15a18bd8c0a8380100ded21c82abd392</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="9" label="rel" layerID="1" created="1374752403151"
+        x="179.44955" y="54.5" width="13.0" height="51.5"
+        strokeWidth="1.0" autoSized="false" controlCount="1"
+        arrowState="3" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/15a18bd9c0a8380100ded21c0bb9e214</URIString>
+        <point1 x="191.73242" y="55.0"/>
+        <point2 x="191.73242" y="105.5"/>
+        <ID1 xsi:type="node">6</ID1>
+        <ID2 xsi:type="node">8</ID2>
+        <ctrlPoint0 x="180.16667" y="80.25" xsi:type="point"/>
+    </child>
+    <layer ID="1" label="Layer 1" created="1374752385383" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/15a18bd9c0a8380100ded21ce65ff9ae</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1374752385382"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B333CC33</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/15a18bdac0a8380100ded21c43baf78b</URIString>
+            <masterSlide ID="2" created="1374752385398" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/15a18bdac0a8380100ded21c57f89ed9</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1374752385410" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/15a18bdac0a8380100ded21c5315357e</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1374752385411" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/15a18bdbc0a8380100ded21c4481fc79</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1374752385412"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/15a18bdbc0a8380100ded21cf4fb5308</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2013-07-25</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>C:\ver\scenetool\test\vue</saveLocation>
+    <saveFile>C:\ver\scenetool\test\vue\05-edge-curved.vue</saveFile>
+</LW-MAP>
