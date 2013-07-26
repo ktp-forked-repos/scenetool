962933ce3e2789a4fa789b239013c7cf76ffcb82
tangentstorm <michal.wallace@gmail.com>
2013-07-26 02:01:38 -0500

    replaced the indentation with a parentid

--- spike/vue2svg.py
+++ spike/vue2svg.py
@@ -10,21 +10,19 @@ def xp(tree, path):
     match = tree.xpath(path, namespaces=nsmap)
     return match[0] if match else ''
 
-
 Join = namedtuple('Join',
-                  ('ntype shape id ts x y w h text layer autosized'
+                  ('parent ntype shape id ts x y w h text layer autosized'
                    ' fill strokewidth strokecolor strokestyle textcolor'
                    ' font id1 id2 p0x p0y p1x p1y ctrlcount arrowstate'
                    ' c0x c0y c1x c1y').split( ))
 
-
-def walk(tree, indent):
+def walk(tree, parent=0):
     """
     walk the tree recursively, extracting node data
     """
     children = tree.xpath('child')
     for child in children:
-        yield indent, Join(*
+        row = Join(*([parent] +
             [xp(child, path) for path in [
                 '@xsi:type',
                 'shape/@xsi:type',
@@ -54,8 +52,9 @@ def walk(tree, indent):
                 'ctrlPoint0/@x',
                 'ctrlPoint0/@y',
                 'ctrlPoint1/@x',
-                'ctrlPoint1/@y' ]])
-        for item in walk(child, indent+1): yield item
+                'ctrlPoint1/@y' ]]))
+        yield row
+        for item in walk(child, row.id): yield item
 
 
 for filename in sorted(glob.glob('*.vue')):
@@ -68,7 +67,6 @@ for filename in sorted(glob.glob('*.vue')):
     vue = etree.parse(io.BytesIO(bytes(data, 'ascii')))
 
     print('##', filename, '#' * (60-len(filename)))
-    for indent, values in walk(vue, 0):
-        print(indent * '> ',
-              '; '.join(values), sep='')
+    for row in walk(vue, 0):
+        print('; '.join(map(str, row)), sep='')
 
