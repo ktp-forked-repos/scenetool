3120c861fe6d49ed3eb91645edf2f15e9549a7e9
tangentstorm <michal.wallace@gmail.com>
2013-07-26 04:05:45 -0500

    vue2svg spike: create an intermediate sqlite3 database

--- spike/vue2svg.py
+++ spike/vue2svg.py
@@ -1,6 +1,10 @@
 import os, glob, io
 from collections import namedtuple
 from lxml import etree
+import sqlite3
+
+
+DB_PATH = ":memory:"
 
 nsmap = {
     'xsi':"http://www.w3.org/2001/XMLSchema-instance"
@@ -57,16 +61,46 @@ def walk(tree, parent=0):
         for item in walk(child, row.id): yield item
 
 
-for filename in sorted(glob.glob('*.vue')):
-
+def load(dbc, filename):
+    """
+    load data from the vue file into the database
+    """
     # vue files are not valid xml because the doctype is misplaced.
     # so to fix, just strip off the opening comments:
     data = open(filename, 'r').read()
     data = data[data.find('<?xml'):]
-
     vue = etree.parse(io.BytesIO(bytes(data, 'ascii')))
 
-    print('##', filename, '#' * (60-len(filename)))
-    for row in walk(vue, 0):
-        print('; '.join(map(str, row)), sep='')
+    cur = dbc.cursor()
+    cur.execute('insert into file values (?)', [filename])
+    fid = cur.lastrowid
 
+    for row in walk(vue, 0):
+        sql = 'insert into shape values (? %s)' \
+            % (', ? ' * len(Join._fields))
+        cur.execute(sql, [fid] + list(row))
+
+def main():
+    dbc = sqlite3.connect(DB_PATH, isolation_level=None) # autocommit
+    cur = dbc.cursor()
+    cur.execute('create table if not exists file (filename string)')
+
+    sql = 'create table if not exists shape (fid integer, %s)' % \
+        ', '.join('%s data' % col for col in Join._fields)
+    cur.execute(sql)
+
+    for filename in sorted(glob.glob('*.vue')):
+        load(dbc,filename)
+
+    cur.execute("select * from file, shape where file.rowid = shape.fid")
+    lastfile = None
+    for row in cur.fetchall():
+        filename = row[0] # from the file table
+        if filename != lastfile:
+            print('##', filename, '#' * (60-len(filename)))
+        print(';'.join(map(str,row[1:])))
+        lastfile = filename
+
+
+if __name__=="__main__":
+    main()
