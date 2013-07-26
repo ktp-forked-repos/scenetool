d4d2dc8c5b32ba6064376be3fe8fa565e2c95cb0
tangentstorm <michal.wallace@gmail.com>
2013-07-26 10:46:04 -0500

    sql script to handle normalizing the table created in vue2svg.py

---
+++ spike/vue2elem.sql
@@ -0,0 +1,76 @@
+-- convert vue data to scene data
+
+-- normalize the fonts:
+insert into font select distinct font from vuedata;
+
+-- and the styles:
+insert into style (fg, bg, sc, sw, font)
+select distinct
+  textcolor, fill, strokecolor, strokewidth,
+  (select rowid from font where font=vuedata.font)
+from vuedata;
+
+-- create a new table with same structure as the input table.
+-- the idea is that we're going to add a bunch of triggers to
+-- this table, and then just copy the data.
+create temp table vue2elem as select * from vuedata where 0=1;
+
+-- temp table to store variables since sqlite doesn't have them
+create temp table kvdict ( k text unique not null, v id );
+
+
+-- trigger to create the common elem record
+create trigger add_vue_data before insert on vue2elem
+  begin
+    -- create an elem record with the common fields
+    insert into elem (scene, style, label, ts, x, y, z)
+    values (
+      new.fid as scene,
+      (select rowid from style
+       where fg = new.textcolor
+	 and bg = new.fill
+         and sc = new.strokecolor
+         and sw = new.strokewidth
+         and font=(select rowid from font where font=new.font)
+       ) as style,
+       new.label, new.ts, new.x, new.y,
+       -- for the z-ordering, just assign numbers in sequence:
+       (select max(z)+1 from elem where scene=new.fid) as z
+    );
+
+    -- remember the rowid of the elem record we just made:
+    insert or replace into kvdict (k,v)
+    values ('last_elem', last_insert_rowid());
+  end;
+
+-- trigger to populate detail table for links/edges
+create trigger add_vue_link after insert on vue2elem
+  when
+    new.ntype = 'link'
+  begin
+    insert into edge (eid, x0, y0, x1, y1)
+    values ((select v from kvdict where k='last_elem') as eid,
+    	    new.x0, new.y0, new.x1, new.y1,
+	    new.cx0, new.cy0, new.cx0, new.cy0);
+  end;
+
+-- trigger to populate detail table for nodes
+create trigger add_vue_node after insert on vue2elem
+  when
+    new.ntype = 'node'
+  begin
+    insert into node (eid, sid, w, h)
+    values ((select v from kvdict where k='last_elem') as eid,
+            (select rowid from shape where shape=new.shape),
+	    new.w, new.h);
+  end;
+
+
+-- now copy the data over, firing the triggers:
+insert into vue2elem select * from vuedata;
+
+-- clean up shop
+-- triggers will be deleted automatically
+drop table kvdict;
+drop table vue2elem;
+
